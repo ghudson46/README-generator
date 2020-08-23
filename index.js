120a429a7960421714ac056592b280df113ab793
Garrett Hudson <garretthudson87@yahoo.com>
2020-08-23 01:08:57 -0400

    added the inquirer functionality to prompt questions

--- index.js
+++ index.js
@@ -1,10 +1,62 @@
-// array of questions for user
-const questions = [
 
-];
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+// array of questions for user
+// const questions = [ "What is the title of your application?", "Describe your application?", "How can a user install this application?", "How does a user use this application?", "What license would you like to use?", "Who contributed to this application?", "What is your github username?", "What is your email?"
+
+// ];
+
+inquirer
+  .prompt([
+    {
+      name: "title",
+      message: "What is the title of your application?",
+      default: "My App",
+    },
+    {
+      name: "description",
+      message: "Describe this application",
+      default: "Cool app that does stuff",
+    },
+    {
+      name: "installation",
+      message: "What does a user have to do to install this application?",
+      default: "nothing"
+    },
+    {
+      name: "usage",
+      message: "How does a user use this application?",
+      default: "Use the command line in node.js",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "What license would you like to use?",
+      choices: ['BSD', 'MIT', 'GPL'],
+    },
+    {
+      name: "contributers",
+      message: "Who else contributed to this application?",
+      default: "Nobody. This is my own work"
+    },
+    {
+      name: "email",
+      message: "What is your email address (in case somebody wants to contact you with questions about this application)",
+      default: "n/a",
+    },
+    {
+      name: "tests",
+      message: "tests?",
+      default: "idk",
+    }
+  ]).then(response => {
+    console.log(response);
+  })
 
 // function to write README file
 function writeToFile(fileName, data) {
+  this.data = data;
 }
 
 // function to initialize program
